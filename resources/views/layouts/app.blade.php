3d738e450d9f01ff337c7818483a963458937f8f
Gimna Katugampala <gimnakatugampala1@gmail.com>
2021-02-28 16:05:02 +0530

    updated the app.blade.php user name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         <ul class="flex items-left">
             <li><a href="" class="p-3">Home</a></li>
             <li><a href="{{route('dashboard')}}" class="p-3">Dashboard</a></li>
-            <li><a href="" class="p-3">Posts</a></li>
+            <li><a href="{{route('posts')}}" class="p-3">Posts</a></li>
         </ul>
 
         <ul class="flex items-center">
